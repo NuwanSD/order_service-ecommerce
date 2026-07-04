7c5db087c8ac51853aca325459cf6f9a879434b7
NuwanSD <sagaradnuwan@gmail.com>
2026-07-04 09:22:21 +0530

    Remove unnecessary @Repository annotation from OrderRepository and add @AutoConfigureJson to OrderControllerTest

--- src/test/java/com/ecommerce/orderservice/controller/OrderControllerTest.java
+++ src/test/java/com/ecommerce/orderservice/controller/OrderControllerTest.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(OrderController.class)
+@AutoConfigureJson
 class OrderControllerTest {
 
     @Autowired
